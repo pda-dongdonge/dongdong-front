e4fc04fa2f8d249eee788a4432d040081abd8214
soyeon <uyyu1734@gmail.com>
2024-05-27 15:57:24 +0900

    design: user page tab 디자인변경

--- src/routes/user/MenuComponent.tsx
+++ src/routes/user/MenuComponent.tsx
@@ -15,16 +15,16 @@ export default function MenuComponent({ tab }: Props) {
     <div className="user-menu flex gap-4 item-center justify-center my-4">
       <Link
         to={`${location.pathname}?tab=bucket`}
-        className={`w-24 mr-12 text-center rounded-lg no-underline text-inherit px-4 py-2 hover:no-underline focus:no-underline ${
-          tab === "bucket" ? `bg-[#7758F6] text-white` : ``
+        className={`w-24 mr-12 text-center no-underline text-inherit px-2 py-2 hover:no-underline focus:no-underline ${
+          tab === "bucket" ? `border-b-4 border-solid border-indigo-500 ` : ``
         }`}
       >
         버킷
       </Link>
       <Link
         to={`${location.pathname}?tab=feed`}
-        className={`w-36 text-center rounded-lg no-underline text-inherit px-4 py-2 hover:no-underline focus:no-underline flex items-center gap-2 ${
-          tab === "feed" ? `bg-[#7758F6] text-white` : ``
+        className={`w-32 text-center no-underline text-inherit px-2 py-2 hover:no-underline focus:no-underline flex justify-center gap-2 ${
+          tab === "feed" ? `border-b-4 border-solid border-indigo-500 ` : ``
         }`}
       >
         <IconHeart />
