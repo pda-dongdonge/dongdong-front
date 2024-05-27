305b6ac7f3e762c90a3e0800d406c725ee8272eb
soyeon <uyyu1734@gmail.com>
2024-05-27 16:18:37 +0900

    design: 레이아웃 container제거하고 따로 스타일링

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -3,12 +3,12 @@ import MyNavbar from "../components/MyNavbar/MyNavbar";
 import { Outlet } from "react-router-dom";
 
 export default function BoardLayout() {
-    return (
-      <>
-        <MyNavbar />
-        <Container style={{ minHeight: "100vh" }}>
-          <Outlet />
-        </Container>
-      </>
-    );
-  }
+  return (
+    <>
+      <MyNavbar />
+      <div className="flex flex-col min-h-screen w-full">
+        <Outlet />
+      </div>
+    </>
+  );
+}
