e914115bd9a37c52e914f6d8d801636a49582a89
gaeon <rkdjs1104@ajou.ac.kr>
2024-05-27 16:42:29 +0900

    ✨feat: 모달에 대한 router 추가

--- src/main-router.tsx
+++ src/main-router.tsx
@@ -3,6 +3,8 @@ import MainPage from "./routes/MainPage";
 import SignPage from "./routes/sign/page";
 import MyNavbar from "./components/MyNavbar/MyNavbar";
 import BoardLayout from "./routes/layout";
+import StoreItems from "./routes/storeItem/page";
+import StoreUrl from "./routes/StoreUrl/StoreUrl";
 const routers = [
   {
     path: "/",
@@ -27,6 +29,17 @@ const routers = [
     ],
     // index: true,
   },
+  {
+    path:"/storeItem",
+    element:<StoreItems/>,
+    index: true,
+  }
+  ,
+  {
+    path:"/storeUrl",
+    element:<StoreUrl/>,
+    index:true
+  }
 
 ];
 const router = createBrowserRouter(routers);
