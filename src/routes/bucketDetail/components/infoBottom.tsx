7cf4863ff28fa182b18d0a8b2322dc920891eef8
kim nahyeon <stome6322@inha.edu>
2024-05-27 16:30:54 +0900

    #1 commnet: 할일 주석 추가

--- src/routes/bucketDetail/components/infoBottom.tsx
+++ src/routes/bucketDetail/components/infoBottom.tsx
@@ -9,10 +9,12 @@ export default function InfoBottom() {
 
   const heartClick = (e:React.ChangeEvent<HTMLInputElement>):void => {
     setValid(prev => !prev);
+    //나중에 로직 추가하기
   }
 
   const shareClick = (e:React.ChangeEvent<HTMLInputElement>):void => {
-    alert("클릭됨");
+    alert("클릭됨"); 
+    //클립보드에 복사되었습니다! 토스트 팝업으로 띄우기 이쁨게
   }
 
   return (
