878a2898f18bc6d7847ab1b8c314fa1ab3e6dca4
kim nahyeon <stome6322@inha.edu>
2024-05-28 16:28:52 +0900

    #16 style: 불필요 파라미터 삭제

--- src/routes/bucketDetail/components/infoBottom.tsx
+++ src/routes/bucketDetail/components/infoBottom.tsx
@@ -7,25 +7,30 @@ import { useState } from "react";
 export default function InfoBottom() {
   const [valid, setValid] = useState<boolean>(false);
 
-  const heartClick = (e:React.ChangeEvent<HTMLInputElement>):void => {
+  const heartClick = (
+    // e:React.ChangeEvent<HTMLInputElement>
+  ):void => {
     setValid(prev => !prev);
     //나중에 로직 추가하기
   }
 
-  const shareClick = (e:React.ChangeEvent<HTMLInputElement>):void => {
+  const shareClick = (
+    // e:React.ChangeEvent<HTMLInputElement>
+  ):void => {
     alert("클릭됨"); 
     //클립보드에 복사되었습니다! 토스트 팝업으로 띄우기 이쁨게
   }
 
   return (
     <>
-      <img className="w-[20px] cursor-pointer" src={shareIcon} onClick={(e) => shareClick(e)}/>
-      <div onClick={(e)=> heartClick(e)}>
+      <img className="w-[20px] cursor-pointer" src={shareIcon} onClick={() => shareClick()}/>
+      <div onClick={()=> heartClick()}>
         {valid ? (
           <img src={heartFillIcon} className="w-[20px] cursor-pointer" />
         ) : (
           <img className="w-[20px] cursor-pointer" src={heartIcon} />
         )}
+        
       </div>
     </>
   );
