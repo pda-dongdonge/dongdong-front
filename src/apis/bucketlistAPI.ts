2b9ccbad7700310073f25d149c0d4d5ec0b98e22
Jueun Kim <jueunkim429@gmail.com>
2024-05-28 11:17:38 +0900

    feat: API생성

--- src/apis/bucketlistAPI.ts
+++ src/apis/bucketlistAPI.ts
@@ -2,10 +2,14 @@ import axios from "axios";
 import { Bucket } from "../store/bucketlist";
 import { BaseApi } from "./baseAPI";
 
-
 export default class bucketlistAPI extends BaseApi {
     async getBucketList(){
         const resp = await this.fetcher.get('/bucket');
+        return resp.data;
+      }
+
+      async getBucketItemUrl(bucketId: number){
+        const resp = await this.fetcher.get(`/bucket/${bucketId}`);
         return resp.data;
       }
 }
