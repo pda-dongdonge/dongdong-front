6dca26efc7b7eb5313ad6b8a10033653594d6bb2
kim nahyeon <stome6322@inha.edu>
2024-05-29 13:16:44 +0900

    #16 feat: ToastContainer app.tsx 추가, dispatch 코드 추가

--- src/routes/bucketDetail/components/infoBottom.tsx
+++ src/routes/bucketDetail/components/infoBottom.tsx
@@ -3,9 +3,12 @@ import shareIcon from "../../../assets/share.svg";
 import heartIcon from "../../../assets/heart.svg";
 import heartFillIcon from "../../../assets/heartFill.svg";
 import { useState } from "react";
+import { showToast } from "@/store/toastPopup";
+import { useDispatch } from "react-redux";
 
 export default function InfoBottom() {
   const [valid, setValid] = useState<boolean>(false);
+  const dispatch = useDispatch();
 
   const heartClick = (): // e:React.ChangeEvent<HTMLInputElement>
   void => {
@@ -16,7 +19,7 @@ export default function InfoBottom() {
   const copyToClipboard = async (): Promise<void> => {
     try {
       await navigator.clipboard.writeText(window.location.href);
-      alert("copied");
+      dispatch(showToast({id: Date.now(), message: "copied!"}));
     } catch (error) {
       alert("failed");
     }
