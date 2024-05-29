d36d68e1b601004184402533aecc91c193bf5890
kim nahyeon <stome6322@inha.edu>
2024-05-29 09:14:21 +0900

    #16 feat: 현재 경로 클립보드에 복사
    함수 추가 복사된 것 확인 완료입니다!

--- src/routes/bucketDetail/components/infoBottom.tsx
+++ src/routes/bucketDetail/components/infoBottom.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import shareIcon from "../../../assets/share.svg";
 import heartIcon from "../../../assets/heart.svg";
 import heartFillIcon from "../../../assets/heartFill.svg";
@@ -7,30 +7,34 @@ import { useState } from "react";
 export default function InfoBottom() {
   const [valid, setValid] = useState<boolean>(false);
 
-  const heartClick = (
-    // e:React.ChangeEvent<HTMLInputElement>
-  ):void => {
-    setValid(prev => !prev);
+  const heartClick = (): // e:React.ChangeEvent<HTMLInputElement>
+  void => {
+    setValid((prev) => !prev);
     //나중에 로직 추가하기
-  }
+  };
 
-  const shareClick = (
-    // e:React.ChangeEvent<HTMLInputElement>
-  ):void => {
-    alert("클릭됨"); 
-    //클립보드에 복사되었습니다! 토스트 팝업으로 띄우기 이쁨게
-  }
+  const copyToClipboard = async (): Promise<void> => {
+    try {
+      await navigator.clipboard.writeText(window.location.href);
+      alert("copied");
+    } catch (error) {
+      alert("failed");
+    }
+  };
 
   return (
     <>
-      <img className="w-[20px] cursor-pointer" src={shareIcon} onClick={() => shareClick()}/>
-      <div onClick={()=> heartClick()}>
+      <img
+        className="w-[20px] cursor-pointer"
+        src={shareIcon}
+        onClick={()=>copyToClipboard()}
+      />
+      <div onClick={() => heartClick()}>
         {valid ? (
           <img src={heartFillIcon} className="w-[20px] cursor-pointer" />
         ) : (
           <img className="w-[20px] cursor-pointer" src={heartIcon} />
         )}
-        
       </div>
     </>
   );
