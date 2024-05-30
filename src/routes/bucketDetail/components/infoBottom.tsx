056fc01b6b36c7f5256b1355182b634d77658025
soyeon <uyyu1734@gmail.com>
2024-05-30 13:47:19 +0900

    fix: 버튼 클릭시 상위 컴포넌트 선택안되도록

--- src/routes/bucketDetail/components/infoBottom.tsx
+++ src/routes/bucketDetail/components/infoBottom.tsx
@@ -7,38 +7,41 @@ import { useBucketlist } from "@/hooks/useBucketlist";
 
 type InfoBottomProps = {
   bucketId: string;
-}
+};
 
-export default function InfoBottom({bucketId}:InfoBottomProps) {
+export default function InfoBottom({ bucketId }: InfoBottomProps) {
   const [valid, setValid] = useState<boolean>(false);
   const dispatch = useDispatch();
-  const {likeBucket, IsLikedBucket} = useBucketlist();
+  const { likeBucket, IsLikedBucket } = useBucketlist();
 
-  useEffect(()=>{
-    const check  = async () => {
-      IsLikedBucket(bucketId)
-      .then(res=> {
+  useEffect(() => {
+    const check = async () => {
+      IsLikedBucket(bucketId).then((res) => {
         // console.log(res.data.isLiked);
         setValid(res?.data.isLiked);
       });
-    }
+    };
     check();
   }, []);
 
-  const heartClick = (): // e:React.ChangeEvent<HTMLInputElement>
+  const heartClick = (
+    e
+  ): // e:React.ChangeEvent<HTMLInputElement>
   void => {
+    e.stopPropagation();
     setValid((prev) => !prev);
-    likeBucket(bucketId).then(res=>{
+    likeBucket(bucketId).then((res) => {
       if (res?.status === 200) {
-        dispatch(showToast({id: Date.now(), message: res.data.message}))
+        dispatch(showToast({ id: Date.now(), message: res.data.message }));
       }
     });
   };
 
-  const copyToClipboard = async (): Promise<void> => {
+  const copyToClipboard = async (e): Promise<void> => {
+    e.stopPropagation();
     try {
       await navigator.clipboard.writeText(window.location.href);
-      const toastId = Date.now()
+      const toastId = Date.now();
       dispatch(showToast({ id: toastId, message: "copied!" }));
       // setTimeout(()=>{
       //   dispatch(removeToast(toastId))
@@ -50,17 +53,21 @@ export default function InfoBottom({bucketId}:InfoBottomProps) {
 
   return (
     <>
-      <FaRegCopy 
-      className="cursor-pointer"
-      color='gray'
-      size='16'
-      onClick={()=>copyToClipboard()}
+      <FaRegCopy
+        className="cursor-pointer"
+        color="gray"
+        size="16"
+        onClick={(e) => copyToClipboard(e)}
       />
-      <div onClick={() => heartClick()}>
+      <div onClick={(e) => heartClick(e)}>
         {valid ? (
-          <IoIosHeart className="cursor-pointer" size='19' color="#ff869b" />
+          <IoIosHeart className="cursor-pointer" size="19" color="#ff869b" />
         ) : (
-          <IoIosHeartEmpty className="cursor-pointer" size='19' color="#ff869b"/>
+          <IoIosHeartEmpty
+            className="cursor-pointer"
+            size="19"
+            color="#ff869b"
+          />
         )}
       </div>
     </>
