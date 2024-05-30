4c04a28e5a77728fd5bdd20915b3c89ff9d7579b
kim nahyeon <stome6322@inha.edu>
2024-05-30 18:49:55 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,20 +7,6 @@ import babel from "@rollup/"
 
 export default defineConfig({
   plugins: [react(), svgr()],
-  server: {
-    proxy: {
-      "/api": {
-        // 요청 전달 대상 서버 주소 설정
-        target: "http://43.203.253.99:1234",
-        // 요청 헤더 host 필드 값을 대상 서버의 호스트 이름으로  변경
-        changeOrigin: true,
-        // 요청 경로에서 '/api' 제거
-        rewrite: (path) => path.replace(/^\/api/, ""),
-        // SSL 인증서 검증 무시
-        secure: false,
-      },
-    },
-  },
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") },
   },
