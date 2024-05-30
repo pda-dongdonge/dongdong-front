7191b13f1e80e5afe9fe2c5280012a6aa9fe4f09
gaeon <rkdjs1104@ajou.ac.kr>
2024-05-30 19:49:00 +0900

    ♻️refactoring: ts변환

--- src/routes/MainPage.tsx
+++ src/routes/MainPage.tsx
@@ -2,14 +2,15 @@ import React, { useState, useEffect } from "react";
 import "./mainpage.css";
 import { useNavigate } from "react-router-dom";
 
-const App = () => {
-  const navigate=useNavigate();
+const App: React.FC = () => {
+  const navigate = useNavigate();
+
   useEffect(() => {
     const handleScroll = () => {
-      const imgElements = document.querySelectorAll(".scroll-image");
+      const imgElements = document.querySelectorAll<HTMLElement>(".scroll-image");
       imgElements.forEach(img => {
         const scrollY = window.scrollY;
-        const speed = img.dataset.scrollSpeed || 0.3;
+        const speed = parseFloat(img.dataset.scrollSpeed || "0.3");
         img.style.transform = `translateY(-${scrollY * speed}px)`;
       });
     };
@@ -20,120 +21,97 @@ const App = () => {
     };
   }, []);
 
-
-
   useEffect(() => {
-    const h1Element = document.querySelector(".fade-text");
+    const h1Element = document.querySelector<HTMLElement>(".fade-text");
     setTimeout(() => {
-      h1Element.style.opacity = 1; // 처음에는 투명하지 않게 설정
+      if (h1Element) h1Element.style.opacity = "1"; // 처음에는 투명하지 않게 설정
     }, 100); // 텍스트가 나타난 후 0.1초 후에 투명하지 않은 상태로 설정
   }, []);
+
   return (
     <>
-<main className="content js-content">
-<section className="block" style={{textAlign:"center", marginBottom:"150px", marginTop:"100px"}}>
-<h1 className="fade-text rounded-text" style={{ fontWeight: "bold", color: "black", height: "100px", opacity: 0 }}>DongdongE에 오신 걸 환영합니다!</h1>
+      <main className="content js-content">
+        <section className="block" style={{ textAlign: "center", marginBottom: "150px", marginTop: "100px" }}>
+          <h1 className="fade-text rounded-text" style={{ fontWeight: "bold", color: "black", height: "100px", opacity: 0 }}>
+            DongdongE에 오신 걸 환영합니다!
+          </h1>
 
-  <div style={{display:"flex", justifyContent:"center"}}>
-   
-  <img className="scroll-image" src="/dongdonglogo.png" alt="로고" width={"250px"} style={{textAlign:"center"}}/>
-  
-  </div>
-</section>
-  <section className="block">
-    <div className="item-parallax-content flex-container img-grid">
-      <figure className="img-gridItem type-right" style={{display:"flex", gap:"20px", justifyContent:"center"}}>
-      <div  className="scroll-image" style={ {borderRadius: "15px",
-                      backgroundImage: `url(https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/YouTube_Logo_%282013-2017%29.svg/800px-YouTube_Logo_%282013-2017%29.svg.png)`,
-                      backgroundSize: "cover",
-                      backgroundPosition: "center",
-                      minWidth: "400px",
-                      minHeight:"170px",
-                      }}>
-
-     </div>
-        <div className="img-caption" >
-          <h1 >Youtube</h1>
-          <p className="scroll-image" style={{color:"#5610A7", fontWeight:"bold"}}>
-            {/* Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle. */}
-            자신의 취향대로,
-          </p>
-        </div>
-      </figure>
-      <figure className="img-gridItem type-left" style={{display:"flex", gap:"20px", justifyContent:"center"}}>
-      <div className="img-caption">
-          <h2 className="head-small">TVING</h2>
-          <p className="scroll-image" style={{color:"#5610A7", fontWeight:"bold"}}>
-            {/* A peep at some distant orb has power to raise and purify our thoughts like a strain of sacred music, or a noble picture, or a passage from the grander poets. */}
-            다양한 OTT의 영상을,
-          </p>
-        </div>
-      <div className="scroll-image" style={ {borderRadius: "15px",
-                      backgroundImage: `url(https://cdn.digitaltoday.co.kr/news/photo/201910/216206_191039_2844.jpg)`,
-                      backgroundSize: "cover",
-                      backgroundPosition: "center",
-                      minWidth: "400px",
-                      minHeight:"170px",
-                      }}>
-
-     </div>
-        {/* <img src="https://cdn.digitaltoday.co.kr/news/photo/201910/216206_191039_2844.jpg" alt="Tving" /> */}
-      
-      </figure>
-    </div>
-  </section>
-  <section className="block">
-    <div className="item-parallax-content flex-container img-grid">
-      <figure className="img-gridItem type-right" style={{display:"flex", gap:"20px", justifyContent:"center"}}>
-      <div className="scroll-image" style={ {borderRadius: "15px",
-                      backgroundImage: `url(https://www.kocca.kr/trend/vol30/img/s11/img_1.jpg)`,
-                      backgroundSize: "cover",
-                      backgroundPosition: "center",
-                      // minWidth: "400px",
-                      minHeight:"170px",
-                      width:"400px",
-                      }}>
-
-     </div>
-        {/* <img src="https://www.kocca.kr/trend/vol30/img/s11/img_1.jpg" alt="neflix" /> */}
-        <figcaption className="img-caption">
-          <h2 >Neflix</h2>
-          <p className="scroll-image" style={{color:"#5610A7", fontWeight:"bold"}}>
-            {/* Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle. */}
-            양동이에 저장하고 공유하는,
-          </p>
-        </figcaption>
-      </figure>
-      <figure className="img-gridItem type-left" style={{display:"flex", gap:"20px", justifyContent:"center"}}>
-        {/* <img src="https://img.wavve.com/service30/profile/wavve.png" alt="wavve" /> */}
-        <figcaption className="img-caption">
-          <h2 className="scroll-image">Wavve</h2>
-          <p className="scroll-image" style={{color:"#5610A7", fontWeight:"bold"}}>
-            {/* A peep at some distant orb has power to raise and purify our thoughts like a strain of sacred music, or a noble picture, or a passage from the grander poets. */}
-            서비스 동동이입니다!
-          </p>
-        </figcaption>
-        <div className="scroll-image" style={ {borderRadius: "15px",
-                      backgroundImage: `url(https://img.wavve.com/service30/profile/wavve.png)`,
-                      backgroundSize: "cover",
-                      backgroundPosition: "center",
-                      // minWidth: "400px",
-                      minHeight:"170px",
-                      width:"400px",
-                      }}>
-
-     </div>
-      </figure>
-    </div>
-  </section>
-  
-  <section className="block section-end" style={{textAlign:"center", marginBottom:"50px"}}>
-   
-        <h1 className="head-large"><div style={{cursor:"pointer", fontWeight:"bold"}} onClick={()=>{
-          navigate('/hot')
-        }} >저희랑 함께 하실래요? | JOIN</div></h1>
-  </section>
-</main>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <img className="scroll-image" src="/dongdonglogo.png" alt="로고" width={"250px"} style={{ textAlign: "center" }} />
+          </div>
+        </section>
+        <section className="block">
+          <div className="item-parallax-content flex-container img-grid">
+            <figure className="img-gridItem type-right" style={{ display: "flex", gap: "20px", justifyContent: "center" }}>
+              <div className="scroll-image" style={{
+                borderRadius: "15px",
+                backgroundImage: `url(https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/YouTube_Logo_%282013-2017%29.svg/800px-YouTube_Logo_%282013-2017%29.svg.png)`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                minWidth: "400px",
+                minHeight: "170px",
+              }}></div>
+              <div className="img-caption">
+                <h1>Youtube</h1>
+                <p className="scroll-image" style={{ color: "#5610A7", fontWeight: "bold" }}>자신의 취향대로,</p>
+              </div>
+            </figure>
+            <figure className="img-gridItem type-left" style={{ display: "flex", gap: "20px", justifyContent: "center" }}>
+              <div className="img-caption">
+                <h2 className="head-small">TVING</h2>
+                <p className="scroll-image" style={{ color: "#5610A7", fontWeight: "bold" }}>다양한 OTT의 영상을,</p>
+              </div>
+              <div className="scroll-image" style={{
+                borderRadius: "15px",
+                backgroundImage: `url(https://cdn.digitaltoday.co.kr/news/photo/201910/216206_191039_2844.jpg)`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                minWidth: "400px",
+                minHeight: "170px",
+              }}></div>
+            </figure>
+          </div>
+        </section>
+        <section className="block">
+          <div className="item-parallax-content flex-container img-grid">
+            <figure className="img-gridItem type-right" style={{ display: "flex", gap: "20px", justifyContent: "center" }}>
+              <div className="scroll-image" style={{
+                borderRadius: "15px",
+                backgroundImage: `url(https://www.kocca.kr/trend/vol30/img/s11/img_1.jpg)`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                minHeight: "170px",
+                width: "400px",
+              }}></div>
+              <figcaption className="img-caption">
+                <h2>Neflix</h2>
+                <p className="scroll-image" style={{ color: "#5610A7", fontWeight: "bold" }}>양동이에 저장하고 공유하는,</p>
+              </figcaption>
+            </figure>
+            <figure className="img-gridItem type-left" style={{ display: "flex", gap: "20px", justifyContent: "center" }}>
+              <figcaption className="img-caption">
+                <h2 className="scroll-image">Wavve</h2>
+                <p className="scroll-image" style={{ color: "#5610A7", fontWeight: "bold" }}>서비스 동동이입니다!</p>
+              </figcaption>
+              <div className="scroll-image" style={{
+                borderRadius: "15px",
+                backgroundImage: `url(https://img.wavve.com/service30/profile/wavve.png)`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                minHeight: "170px",
+                width: "400px",
+              }}></div>
+            </figure>
+          </div>
+        </section>
+        <section className="block section-end" style={{ textAlign: "center", marginBottom: "50px" }}>
+          <h1 className="head-large">
+            <div style={{ cursor: "pointer", fontWeight: "bold" }} onClick={() => {
+              navigate('/hot');
+            }}>저희랑 함께 하실래요? | JOIN</div>
+          </h1>
+        </section>
+      </main>
     </>
   );
 };
